4fd6f9c7222d5b8984c290b1eaf9a4159d935aeb
IronFirst89 <pizza4dogncat@gmail.com>
2026-03-25 10:09:07 +0800

    Commit here

--- HelloApp/obj/Release/net10.0/HelloApp.AssemblyInfo.cs
+++ HelloApp/obj/Release/net10.0/HelloApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39d2c196b99f32a38ac956f3c094316c66bb546e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f56eda42a1c8184a3671fb5f521ff95a32f2467")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
